c3e1594fb36aa70934ed9462716af9c5e2a7418f
Eric Brehault <ebrehault@gmail.com>
2018-08-03 10:35:41 +0200

    manage contexts

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.local.set({ record: false });
-    chrome.storage.local.set({ replay: false });
-});
+// chrome.runtime.onInstalled.addListener(function() {
+//     chrome.storage.local.set({ record: false });
+//     chrome.storage.local.set({ replay: false });
+// });
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.request) {
         recordRequest(request.request);
@@ -12,15 +12,30 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 var count = 0;
 var debuggee;
 var requests = {};
+var hostnames = {};
 var exactMatch = {};
 var nearMatch = {};
 var origin;
-var context = 'CONTEXT';
+var context;
+var status = 'idle';
 
-function startRecord() {
-    console.log('Start recording');
+function getStatus() {
+    return status;
+}
+
+function getContext() {
+    return context;
+}
+
+function getContexts() {
+    return JSON.parse(localStorage.getItem('contexts') || '[]');
+}
+
+function startRecord(contextName) {
+    console.log('Start recording ' + contextName);
+    status = 'recording';
+    context = contextName;
     chrome.browserAction.setIcon({ path: 'record.png' });
-    chrome.storage.local.set({ record: true });
     chrome.tabs.executeScript({
         file: 'content.js'
     });
@@ -28,20 +43,27 @@ function startRecord() {
 
 function stopRecord() {
     chrome.browserAction.setIcon({ path: 'icon.png' });
-    chrome.storage.local.set({ record: false });
-    localStorage.setItem('repeat-after-' + context, JSON.stringify(requests));
+    status = 'idle';
+    localStorage.setItem('requests-' + context, JSON.stringify(requests));
+    localStorage.setItem('hostnames-' + context, JSON.stringify(hostnames));
+    var contexts = JSON.parse(localStorage.getItem('contexts') || '[]');
+    if (contexts.indexOf(context) === -1) {
+        contexts.push(context);
+        localStorage.setItem('contexts', JSON.stringify(contexts));
+    }
 }
 
-function startPlay() {
+function startPlay(contextName) {
     chrome.browserAction.setIcon({ path: 'play.png' });
-    chrome.storage.local.set({ play: true });
+    status = 'playing';
+    context = contextName;
     loadRequests();
     startProxy();
 }
 
 function stopPlay() {
     chrome.browserAction.setIcon({ path: 'icon.png' });
-    chrome.storage.local.set({ play: false });
+    status = 'idle';
     chrome.debugger.attach(debuggee, '1.2', () => {
         chrome.debugger.sendCommand(debuggee, 'Network.setRequestInterceptionEnabled', { enabled: false });
     });
@@ -51,10 +73,12 @@ function stopPlay() {
 function recordRequest(request) {
     requests[count] = request;
     count ++;
+    hostnames[getHost(request.url)] = true;
 }
 
 function loadRequests() {
-    requests = JSON.parse(localStorage.getItem('repeat-after-' + context) || '{}');
+    requests = JSON.parse(localStorage.getItem('requests-' + context) || '{}');
+    hostnames = JSON.parse(localStorage.getItem('hostnames-' + context) || '{}');
     Object.keys(requests).forEach(id => {
         var request = requests[id];
         exactMatch[getExactKey(request)] = id;
@@ -80,11 +104,11 @@ function getNearKey(request) {
 }
 
 function startProxy() {
-    console.log('START');
+    console.log('Start playing ' + context);
     chrome.debugger.getTargets((targets) => {
         let target = targets.find(t => t.type === 'page' && t.attached);
         debuggee = { targetId: target.id };
-        origin = target.url.split('/').slice(0,3).join('/');
+        origin = getHost(target.url);
     
         chrome.debugger.attach(debuggee, '1.2', () => {
             chrome.debugger.sendCommand(debuggee, 'Network.setRequestInterception', {patterns: [{ urlPattern: '*' }]});
@@ -96,35 +120,35 @@ function startProxy() {
 
 function rewriteResponse(source, method, params) {
     if(source.targetId === debuggee.targetId && method === 'Network.requestIntercepted') {
-        console.log('YOP', params.request);
         var request = params.request;
         var continueParams = {
             interceptionId: params.interceptionId,
         };
-        var responseLines = [];
-        if (request.method !== 'OPTIONS') {
-            response = getBestResponse(request);
-            responseLines.push(`HTTP/1.1 ${response.status}`);
-            responseLines.push(`Access-Control-Allow-Origin: ${origin}`);
-            responseLines.push('Access-Control-Allow-Credentials: true');
-            responseLines.push('Content-Type: application/json');
-            responseLines.push(`Content-Length: ${response.size}`);
-            responseLines.push(`Repeat-After-Me-Context: ${context}`);
-            responseLines.push('');
-            responseLines.push('');
-            responseLines.push(response.body);
-        } else {
-            responseLines.push('HTTP/1.1 200 OK');
-            responseLines.push(`Access-Control-Allow-Origin: ${origin}`);
-            responseLines.push('Access-Control-Allow-Credentials: true');
-            responseLines.push('Content-Length: 0');
-            responseLines.push(`Repeat-After-Me-Context: ${context}`);
-            responseLines.push('Access-Control-Allow-Headers: Accept, Authorization, Content-Type, X-Custom-Header, DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control');
-            responseLines.push('');
-            responseLines.push('');
+        if (isCachedHost(request.url)) {
+            var responseLines = [];
+            if (request.method !== 'OPTIONS') {
+                response = getBestResponse(request);
+                responseLines.push(`HTTP/1.1 ${response.status}`);
+                responseLines.push(`Access-Control-Allow-Origin: ${origin}`);
+                responseLines.push('Access-Control-Allow-Credentials: true');
+                responseLines.push('Content-Type: application/json');
+                responseLines.push(`Content-Length: ${response.size}`);
+                responseLines.push(`Repeat-After-Me-Context: ${context}`);
+                responseLines.push('');
+                responseLines.push('');
+                responseLines.push(response.body);
+            } else {
+                responseLines.push('HTTP/1.1 200 OK');
+                responseLines.push(`Access-Control-Allow-Origin: ${origin}`);
+                responseLines.push('Access-Control-Allow-Credentials: true');
+                responseLines.push('Content-Length: 0');
+                responseLines.push(`Repeat-After-Me-Context: ${context}`);
+                responseLines.push('Access-Control-Allow-Headers: Accept, Authorization, Content-Type, X-Custom-Header, DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control');
+                responseLines.push('');
+                responseLines.push('');
+            }
+            continueParams.rawResponse = btoa(responseLines.join('\r\n'));
         }
-        continueParams.rawResponse = btoa(responseLines.join('\r\n'));
-        console.log('yep', continueParams);
         chrome.debugger.sendCommand(debuggee, 'Network.continueInterceptedRequest', continueParams);
     }
 }
@@ -148,4 +172,12 @@ function getBestResponse(request) {
             size: 2,
         };
     }
+}
+
+function getHost(url) {
+    return url.split('/').slice(0,3).join('/');
+}
+
+function isCachedHost(url) {
+    return hostnames[getHost(url)];
 }
--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ var contextInput = document.getElementById('context-input');
 var contextSelect = document.getElementById('context-select');
 
 var record = function() {
-    chrome.extension.getBackgroundPage().startRecord();
+    chrome.extension.getBackgroundPage().startRecord(context);
     recordStopBtn.disabled = true;
     recordStopBtn.disabled = false;
     playBtn.disabled = true;
@@ -22,10 +22,14 @@ var stopRecord = function() {
     stopBtn.disabled = true;
 };
 var play = function() {
-    chrome.extension.getBackgroundPage().startPlay();
+    chrome.extension.getBackgroundPage().startPlay(context);
+    playBtn.disabled = true;
+    stopBtn.disabled = false;
 };
 var stopPlay = function() {
     chrome.extension.getBackgroundPage().stopPlay();
+    playBtn.disabled = false;
+    stopBtn.disabled = true;
 };
 
 recordBtn.addEventListener('click', record);
@@ -38,3 +42,30 @@ contextInput.addEventListener('keyup', function(e) {
         recordBtn.disabled = false;
     }
 });
+contextSelect.addEventListener('change', function(e) {
+    context = e.target.selectedOptions[0].value;
+    if (context) {
+        playBtn.disabled = false;
+    }
+});
+
+var status = chrome.extension.getBackgroundPage().getStatus();
+if (status === 'recording') {
+    recordStopBtn.disabled = true;
+    recordStopBtn.disabled = false;
+}
+if (status === 'playing') {
+    playBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+context = chrome.extension.getBackgroundPage().getContext();
+if (context) {
+    contextInput.value = context;
+    if (status === 'idle') {
+        recordBtn.disabled = false;
+    }
+}
+var options = chrome.extension.getBackgroundPage().getContexts().map(contextName => {
+    return `<option value="${contextName}">${contextName}</option>`;
+});
+contextSelect.innerHTML = '<option value="">Select a context</option>' + options.join('');
